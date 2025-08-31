a42696c4ad6fc1b0963310b06b41b64a6cbf9494
Sean Murphy <murphysean84@gmail.com>
2025-08-30 21:01:59 -0700

    Will read from and write to the current buffer

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use libuv::{
     AsyncHandle, Buf, BufTrait, ConnectCB, HandleTrait, Loop, ReadonlyBuf, StreamTrait, TcpHandle,
 };
 use libuv_sys2::uv_loop_t;
-use nvim_oxi::api::{self, Window, notify, opts::*, types::*};
+use nvim_oxi::api::{self, notify, opts::*, types::*};
 use nvim_oxi::lua::ffi::State;
 use nvim_oxi::lua::with_state;
 use nvim_oxi::{Dictionary, Function, Object, print, schedule};
@@ -151,8 +151,6 @@ impl SeanVim {
     }
 }
 
-//Write a rust function that iterates over a range of numbers from 0 to 10
-
 type ReadTup = (usize, usize, ReadonlyBuf);
 
 //Let's make an http call
@@ -186,8 +184,8 @@ impl MyTcp {
     pub async fn connect(&mut self, addr: SocketAddr) -> Result<u32, libuv::Error> {
         let ccb = ConnectFuture::new();
         let r = self.handle.connect(&addr, &ccb);
-        if let Err(e) = r {
-            eprintln!("ERRORS HAPPEN:connect: {}", e);
+        if let Err(_e) = r {
+            //eprintln!("ERRORS HAPPEN:connect: {}", e);
             return Err(libuv::Error::EFAULT);
         }
         ccb.await
@@ -238,25 +236,25 @@ impl AsyncRead for MyTcp {
                 ),
             );
             if let Err(e) = r {
-                eprintln!("ERRORS HAPPEN:read_start: {}", e);
+                //eprintln!("ERRORS HAPPEN:read_start: {}", e);
                 return Poll::Ready(Err(std::io::Error::other(e)));
             }
         }
 
-        eprintln!("Read0");
+        //eprintln!("Read0");
         if mut_self.read_waker.borrow().is_some() {
-            eprintln!("Read1");
+            //eprintln!("Read1");
             return Poll::Ready(Err(std::io::Error::from(ErrorKind::AlreadyExists)));
         }
 
-        eprintln!("Read2");
+        //eprintln!("Read2");
         if mut_self.reads.borrow().is_empty() {
-            eprintln!("Read3");
+            //eprintln!("Read3");
             mut_self.read_waker.borrow_mut().replace(cx.waker().clone());
             return Poll::Pending;
         }
 
-        eprintln!("Read4");
+        //eprintln!("Read4");
         Poll::Ready(copy_into_from_vec_buffs(
             buf,
             &mut mut_self.reads.borrow_mut(),
@@ -280,15 +278,11 @@ fn copy_into_from_vec_buffs(
             //If there is less (or equal) incoming than room in my buffer, consume it all and copy into incoming
             //and return bytes_consumed
             if (*len - *start) <= into_buff.len() - into_buff_bytes_read {
-                eprintln!(
-                    "Copy1C: ({},{}) -> ({},{})",
-                    *start,
-                    *len,
-                    into_buff_bytes_read,
-                    *len - *start,
-                );
+                //eprintln!( "Copy1C: ({},{}) -> ({},{})", *start, *len, into_buff_bytes_read, *len - *start,);
                 for i in 0..into_buff.len() {
-                    if i >= *len{break} 
+                    if i >= *len {
+                        break;
+                    }
                     into_buff[i] = b[*start + i];
                 }
                 //into_buff[into_buff_bytes_read..*len - *start - 1]
@@ -312,12 +306,7 @@ fn copy_into_from_vec_buffs(
             }
         }
         Some(Err(e)) => {
-            eprintln!(
-                "Copy2: br: {},{} e: {}",
-                into_buff_bytes_read,
-                into_buff.len(),
-                e
-            );
+            //eprintln!( "Copy2: br: {},{} e: {}", into_buff_bytes_read, into_buff.len(), e);
             if let libuv::EOF = e {
                 //eprintln!("Returning: {}, underlying slice contains: {:?}", into_buff_bytes_read, &into_buff[0..into_buff_bytes_read]);
                 return Ok(into_buff_bytes_read);
@@ -330,7 +319,7 @@ fn copy_into_from_vec_buffs(
         _ => {}
     }
 
-    eprintln!("Copy4: br: {},{}", into_buff_bytes_read, into_buff.len());
+    //eprintln!("Copy4: br: {},{}", into_buff_bytes_read, into_buff.len());
     //eprintln!("Copy4: first 6 bytes: {:?}", &into_buff[0..6]);
     Ok(into_buff_bytes_read)
 }
@@ -345,11 +334,7 @@ impl AsyncWrite for MyTcp {
         buf: &[u8],
     ) -> Poll<std::io::Result<usize>> {
         //Need to dump the bytes and return ready for this buffer
-        eprintln!(
-            "Writing {} bytes: {}",
-            buf.len(),
-            std::str::from_utf8(buf).unwrap()
-        );
+        //eprintln!( "Writing {} bytes: {}", buf.len(), std::str::from_utf8(buf).unwrap());
         let mut bufsn = Buf::new_from_bytes(buf).unwrap();
         let mut bufs = Buf::new_from(&bufsn, Some(buf.len())).unwrap();
         let writes = self.writes.clone();
@@ -375,7 +360,7 @@ impl AsyncWrite for MyTcp {
         self: std::pin::Pin<&mut Self>,
         cx: &mut Context<'_>,
     ) -> Poll<std::io::Result<()>> {
-        eprintln!("FLUSHING!!!");
+        //eprintln!("FLUSHING!!!");
         if self.writes.borrow().is_empty() {
             return Poll::Ready(Ok(()));
         }
@@ -391,7 +376,7 @@ impl AsyncWrite for MyTcp {
         self: std::pin::Pin<&mut Self>,
         cx: &mut Context<'_>,
     ) -> Poll<std::io::Result<()>> {
-        eprintln!("Closing");
+        //eprintln!("Closing");
         if self.close_waker.borrow().is_some() {
             return Poll::Ready(Err(std::io::Error::from(ErrorKind::AlreadyExists)));
         }
@@ -466,30 +451,13 @@ fn get_lua_loop() -> Loop {
         handle: unsafe { with_state(|state| luv_loop(state)) },
         should_drop: false,
     };
-    eprintln!("LLPTR: {:?}", lua_loop.handle);
+    //eprintln!("LLPTR: {:?}", lua_loop.handle);
     unsafe { mem::transmute(lua_loop) }
 }
 
 #[nvim_oxi::plugin]
 pub fn seanvim() -> nvim_oxi::Result<Dictionary> {
-    let greetings = |args: CommandArgs| {
-        let who = args.args.unwrap_or("from  Rust".to_owned());
-        let bang = if args.bang { "!" } else { "" };
-        eprintln!("Hello {}{}", who, bang);
-    };
-
-    api::create_user_command(
-        "SeanVim",
-        greetings,
-        &CreateCommandOpts::builder()
-            .bang(true)
-            .desc("shows a greetings message")
-            .nargs(CommandNArgs::ZeroOrOne)
-            .build(),
-    )?;
-
-    //This is getting annoying
-    //api::set_keymap(Mode::Insert, "hi", "hello", &Default::default())?;
+    let mut api: Dictionary = Dictionary::new();
 
     let sean_vim: Rc<RefCell<SeanVim>> = Rc::new(RefCell::new(SeanVim::new()));
     let uninit_waker = LuvWaker::new();
@@ -510,17 +478,12 @@ pub fn seanvim() -> nvim_oxi::Result<Dictionary> {
     //Set up my app to be able to clone the waker for runs
     sean_vim.borrow_mut().waker = Some(waker);
 
-    // Creates two functions `{open,close}_window` to open and close a
-    // floating window.
-    let mut buf = api::create_buf(false, true)?;
-    let win: Rc<RefCell<Option<Window>>> = Rc::default();
-    let _ = buf.set_name("SeanVimBuff");
-
     let sv = sean_vim.clone();
     let setup: Function<Dictionary, Result<(), api::Error>> =
         Function::from_fn(move |opts: Dictionary| {
             let mut svi = sv.borrow_mut();
             svi.setup(opts);
+            print!("SEANVIM is setup");
             Ok(())
         });
 
@@ -545,187 +508,129 @@ pub fn seanvim() -> nvim_oxi::Result<Dictionary> {
             .build(),
     )?;
 
-    let w = Rc::clone(&win);
-    let b = buf.clone();
-    let open_window: Function<(), Result<(), api::Error>> = Function::from_fn(move |()| {
-        if w.borrow().is_some() {
-            api::err_writeln("Window is already open");
-            return Ok(());
-        }
+    let ollama_generate: Function<(), Result<(), api::Error>> = Function::from_fn(move |()| {
+        let _ = sean_vim.borrow().spawner().spawn_local(async move {
+            let l = get_lua_loop();
+            let mut my_tcp = MyTcp::new(&l);
+            let addr: SocketAddr = SocketAddr::from_str("192.168.254.37:11434").unwrap();
+            //let addr: SocketAddr = SocketAddr::from_str("104.248.184.153:80").unwrap();
 
-        let config = WindowConfig::builder()
-            .relative(WindowRelativeTo::Cursor)
-            .height(5)
-            .width(50)
-            .row(1)
-            .col(0)
-            .build();
+            let r = my_tcp.connect(addr).await;
+            match r {
+                Ok(_r) => {},
+                Err(e) => print!("Connected? {}", e),
+            }
+            if r.is_err() {
+                return;
+            }
 
-        let mut win = w.borrow_mut();
-        *win = Some(api::open_win(&b, false, &config)?);
+            let mut system_prompt : Vec<String> = vec![
+                "You are a Software Engineer specializing in Rust.".into(),
+            ];
 
+            let mut instructions: Vec<String> = vec![
+                "What follows are 15 lines of code provided to you".into(),
+                "Provide rust code with no explanation (unless you code comment it)".into(),
+                "Do not wrap it or escape it, it will be directly inserted into the document at the end of the following context".into(),
+
+            ];
+
+            //Get the previous X lines out of the buffer to send
+            let cursor = nvim_oxi::api::get_current_win().get_cursor().unwrap();
+            let start_line = std::cmp::max(cursor.0 - 10, 0);
+            let buffer = nvim_oxi::api::get_current_buf();
+            let lines = buffer.get_lines(start_line..cursor.0, false).unwrap();
+            let mut code_context: Vec<String> = lines
+                .map(|ns| {
+                    let s = ns.to_str().unwrap_or_default();
+                    String::from(s)
+                })
+                .collect::<Vec<String>>();
+
+            let mut prompt : Vec<String> = Vec::new();
+            prompt.append(&mut system_prompt);
+            prompt.append(&mut instructions);
+            prompt.push("```".into());
+            prompt.append(&mut code_context);
+            prompt.push("```".into());
+            let prompt_string :String = prompt.join("\n");
+
+            /*
+            //TODO NEXT I'd like to get the selected text here and pass it to the llm with some context
+            if let Ok(mode) = nvim_oxi::api::get_mode() {
+                if mode.mode == "V" {
+                    let sp = nvim_oxi::api::call_function::<(&str,), (i32, i32, i32, i32)>(
+                        "getpos",
+                        ("v",),
+                    );
+                    let ep = nvim_oxi::api::call_function::<(&str,), (i32, i32, i32, i32)>(
+                        "getpos",
+                        (".",),
+                    );
+                    if sp.is_ok() && ep.is_ok() {
+                        let sp = sp.unwrap();
+                        let ep = ep.unwrap();
+                        let ep = nvim_oxi::api::call_function::<
+                            (i32, i32, i32, i32, i32, i32, i32, i32),
+                            String,
+                        >(
+                            "getregion",
+                            (sp.0, sp.1, sp.2, sp.3, ep.0, ep.1, ep.2, ep.3),
+                        );
+                        if ep.is_ok() {
+                            print!("Wow, here {}", ep.unwrap());
+                        }
+                    }
+                }
+            }
+            */
+
+            let mut my_req = Request::post("http://seanvim.requestcatcher.com/api/generate");
+            my_req.insert_header("User-Agent", "nvim-libuv");
+            my_req.insert_header("Accept", "application/json");
+            my_req.insert_header("Content-Type", "application/json");
+            my_req.insert_header("Connection", "close");
+            let rb = json!({
+                "model": "gemma3:12b",
+                "prompt": prompt_string,
+                "stream": false
+            });
+            my_req.set_body(rb);
+            let r = async_h1::connect(my_tcp, my_req).await;
+            match r {
+                Ok(mut res) => {
+                    //print!("Response: {:?}", res);
+                    let b = res.take_body();
+                    //let s = b.into_string().await.unwrap();
+                    //print!("Response Body: {}", s);
+                    let j_r = b.into_json::<OllamaGenerateResponse>().await;
+                    //print!("Response: {:?}", j_r);
+                    if let Ok(j) = j_r {
+                        //print!("Response Body: {}", j.response);
+                        print!("here");
+                        let cursor = nvim_oxi::api::get_current_win().get_cursor().unwrap();
+                        let mut buffer = nvim_oxi::api::get_current_buf();
+                        let r = buffer.set_lines(
+                            cursor.0..cursor.0, 
+                            false, 
+                            j.response
+                                .lines()
+                                .map(|s|{nvim_oxi::String::from(s)})
+                                .collect::<Vec<nvim_oxi::String>>()
+                        );
+                        print!("respone {}", r.is_ok());
+                    }
+                }
+                Err(e) => print!("Response: ERROR: {:?}", e),
+            }
+        });
         Ok(())
     });
 
-    let w = Rc::clone(&win);
-    let b = buf.clone();
-    let sean_open = move |_args: CommandArgs| {
-        if w.borrow().is_some() {
-            api::err_writeln("Window is already open");
-            return;
-        }
+    print!("SEANVIM is initialized");
 
-        let config = WindowConfig::builder()
-            .relative(WindowRelativeTo::Cursor)
-            .height(5)
-            .width(10)
-            .row(1)
-            .col(0)
-            .build();
-
-        let mut win = w.borrow_mut();
-        *win = Some(api::open_win(&b, false, &config).unwrap());
-    };
-
-    api::create_user_command(
-        "SeanOpen",
-        sean_open,
-        &CreateCommandOpts::builder()
-            .bang(true)
-            .desc("opens a window")
-            .nargs(CommandNArgs::ZeroOrOne)
-            .build(),
-    )?;
-
-    let w = Rc::clone(&win);
-    let close_window: Function<(), Result<(), api::Error>> = Function::from_fn(move |()| {
-        if w.borrow().is_none() {
-            api::err_writeln("Window is already closed");
-            return Ok(());
-        }
-
-        let win = w.borrow_mut().take().unwrap();
-        win.close(false)
-    });
-
-    let w = Rc::clone(&win);
-    let sean_close = move |_args: CommandArgs| {
-        if w.borrow().is_none() {
-            api::err_writeln("Window is already closed");
-            return Ok(());
-        }
-
-        let win = w.borrow_mut().take().unwrap();
-        win.close(false)
-    };
-
-    api::create_user_command(
-        "SeanClose",
-        sean_close,
-        &CreateCommandOpts::builder()
-            .bang(true)
-            .desc("closes a window")
-            .nargs(CommandNArgs::ZeroOrOne)
-            .build(),
-    )?;
-
-    let mut api: Dictionary = Dictionary::new();
     api.insert("setup", setup);
-    api.insert("open_window", open_window);
-    api.insert("close_window", close_window);
-
-    let _ = sean_vim.borrow().spawner().spawn_local(async {
-        let _ = notify("Hello", LogLevel::Info, &Dictionary::new());
-    });
-
-    api::create_user_command(
-        "SeanOllama",
-        move |args: CommandArgs| {
-            let msg = args
-                .args
-                .unwrap_or("can you introduce yourself?".to_owned());
-            let _ = sean_vim.borrow().spawner().spawn_local(async move {
-                let l = get_lua_loop();
-                let mut my_tcp = MyTcp::new(&l);
-                let addr: SocketAddr = SocketAddr::from_str("192.168.254.37:11434").unwrap();
-                //let addr: SocketAddr = SocketAddr::from_str("104.248.184.153:80").unwrap();
-
-                let r = my_tcp.connect(addr).await;
-                let _ = match r {
-                    Ok(r) => notify(
-                        &format!("Connected! {}", r),
-                        LogLevel::Info,
-                        &Dictionary::new(),
-                    ),
-                    Err(e) => notify(
-                        &format!("Connected? {}", e),
-                        LogLevel::Info,
-                        &Dictionary::new(),
-                    ),
-                };
-                if r.is_err() {
-                    return;
-                }
-
-                //TODO NEXT I'd like to get the selected text here and pass it to the llm with some context
-                if let Ok(mode) = nvim_oxi::api::get_mode() {
-                    if mode.mode == "V" {
-                        let sp = nvim_oxi::api::call_function::<(&str,), (i32,i32,i32,i32)>(
-                            "getpos", ("v",)
-                        );
-                        let ep = nvim_oxi::api::call_function::<(&str,), (i32,i32,i32,i32)>(
-                            "getpos", (".",)
-                        );
-                        if sp.is_ok() && ep.is_ok(){
-                            let sp = sp.unwrap();
-                            let ep = ep.unwrap();
-                        let ep = nvim_oxi::api::call_function::<(i32,i32,i32,i32,i32,i32,i32,i32), String>(
-                            "getregion", (sp.0,sp.1,sp.2,sp.3,ep.0,ep.1,ep.2,ep.3)
-                        );
-                            if ep.is_ok(){
-                        print!("Wow, here {}", ep.unwrap());}
-                        }
-                    }
-                }
-
-                let mut my_req = Request::post("http://seanvim.requestcatcher.com/api/generate");
-                my_req.insert_header("User-Agent", "nvim-libuv");
-                my_req.insert_header("Accept", "application/json");
-                my_req.insert_header("Content-Type", "application/json");
-                my_req.insert_header("Connection", "close");
-                let rb = json!({
-                    "model": "gemma3:12b",
-                    "prompt": "can you introduce yourself?",
-                    "stream": false
-                });
-                my_req.set_body(rb);
-                let r = async_h1::connect(my_tcp, my_req).await;
-                match r {
-                    Ok(mut res) => {
-                        print!("Response: {:?}", res);
-                        let b = res.take_body();
-                        //let s = b.into_string().await.unwrap();
-                        //print!("Response Body: {}", s);
-                        let j_r = b.into_json::<OllamaGenerateResponse>().await;
-                        print!("Response: {:?}", j_r);
-                        if let Ok(j) = j_r {
-                            print!("Response Body: {}", j.response);
-                            let _ = nvim_oxi::api::call_function::<(&str, &str), ()>(
-                                "setreg",
-                                ("o", j.response.as_str()),
-                            );
-                        }
-                    }
-                    Err(e) => print!("Response: ERROR: {:?}", e),
-                }
-            });
-        },
-        &CreateCommandOpts::builder()
-            .bang(true)
-            .desc("dumps an ollama call into the o register")
-            .nargs(CommandNArgs::ZeroOrOne)
-            .build(),
-    )?;
+    api.insert("ollama_generate", ollama_generate);
 
     Ok(api)
 }
